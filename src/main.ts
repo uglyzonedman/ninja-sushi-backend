f59bcd6afb21e274c52536f16edfddc8806849d8
Dmitry Moiseenko <majest228@gmail.com>
2024-11-08 11:58:18 +0500

    Фикс

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
     'http://localhost:4200',
   ];
   app.enableCors({ credentials: true, origin: allowedOrigins });
-  await app.listen(8081);
+  await app.listen(8080);
 }
 bootstrap();
